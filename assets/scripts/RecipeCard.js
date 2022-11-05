d5f4ddca89e9afb77dbd20dee0dd016c2096bfc8
scottieboyzhang <97482103+scottieboyzhang@users.noreply.github.com>
2022-11-05 14:42:13 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,10 +8,17 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    const shadowOpen = this.attachShadow({ mode: "open" });
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+    const eArticle = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+    const fileStyle = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+    const cardStyle = document.querySelector('style');
+    fileStyle.append(cardStyle);
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+    shadowOpen.append(fileStyle);
+    shadowOpen.append(eArticle);
   }
 
   /**
@@ -40,6 +47,7 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+    const art = document.querySelector('article');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
